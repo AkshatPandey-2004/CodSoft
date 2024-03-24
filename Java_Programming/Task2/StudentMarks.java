ab68d7c94d51435ce506db09954f2db9c2a5b0f1
Akshat Pandey <akshatpandey26.2004@gmail.com>
2024-03-24 19:40:55 +0530

    Create StudentMarks.java

---
+++ Java_Programming/Task2/StudentMarks.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class StudentMarks {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        char grade = calculateGrade(averagePercentage);
+
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + String.format("%.2f", averagePercentage) + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    public static char calculateGrade(double percentage) {
+        if (percentage >= 85) {
+            return 'A' + '+'; 
+        } else if (percentage >= 75) {
+            return 'A';
+        } else if (percentage >= 65) {
+            return 'B' + '+';
+        } else if (percentage >= 55) {
+            return 'B';
+        } else if (percentage >= 45) {
+            return 'C' + '+';
+        } else if (percentage >= 35) {
+            return 'C';
+        } else {
+            return 'F';
+        }
+    }
+}
